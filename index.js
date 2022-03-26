b73c2ed3c3a6ab53418e59cb7500d9ad6d6f4016
Magen Albritton <magenelise@gmail.com>
2022-03-26 17:27:55 -0500

    commit tree 2d8d96a085c1aa8ab0a13bf3f0ea64cef21c4bfc

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your code here!
+
+
+
+
+
+
+
+
+
+
+
+
+const element = document.getElementById("main")
+element.remove()
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+
+newHeader.textContent = "Magen is the champion"
+document.body.append(newHeader)
